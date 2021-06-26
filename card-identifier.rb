6be48e48b4fea07e266092fbe03f7e2bf0e67ed6
Samuel Wilson <samWson@users.noreply.github.com>
2021-06-26 15:38:17 +1200

    Identify card types
    Ending the exercise here as I have already spent the 1.5 hours time on
    this.
    
    I haven't been able to get the tests running. When the test case is run
    from the command line no assertions are made.

--- card-identifier.rb
+++ card-identifier.rb
@@ -5,25 +5,53 @@
 class CardIdentifier
   PREFIXES = {
     'Black' => '60141',
-    'Red =>' => '6014352',
+    'Red' => '6014352',
     'Green_a' => '6014355526',
     'Green_b' => '6014355529',
     'Blue' => '6014'
   }
+
+  # A pipeline of methods to be run against the card in order. The card will be identified with the first that returns true.
+  CARD_IDENTIFIERS = %w[identify_black identify_red identify_green identify_blue]
 
   def initialize(number)
     @number = number
   end
 
   def identify
-    identified_card = ''
-    
-    PREFIXES.each do |key, value|
-      if @number.start_with?(value)
-        identified_card = "Fly Buys #{key}: #{@number}"
-      end
-    end
+    identified_as = CARD_IDENTIFIERS.detect do |identifier|
+      self.send(identifier)
+    end 
 
-    identified_card
+    case identified_as
+    when 'identify_black'
+      "Fly buys Black: #{@number}"
+    when 'identify_red'
+      "Fly buys Red: #{@number}"
+    when 'idenfity_green'
+      "Fly buys Green: #{@number}"
+    when 'identify_blue'
+      "Fly buys Blue: #{@number}"
+    else
+      "Unknown: #{@number}"
+    end
+  end
+
+  private
+
+  def identify_black
+    @number.start_with?(PREFIXES['Black']) && (@number.size == 16 || @number.size == 17) 
+  end
+
+  def identify_red
+    @number.start_with?(PREFIXES['Red']) && @number.size == 16
+  end
+
+  def identify_green
+    (@number.start_with?(PREFIXES['Green_a']) || @number.start_with?(PREFIXES['Green_b'])) && @number.size == 16
+  end
+
+  def identify_blue
+    @number.start_with?(PREFIXES['Blue']) && @number.size == 16
   end
 end

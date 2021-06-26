0540eb9f4a3a023c570b6911a4914c8c513c8aac
Samuel Wilson <samWson@users.noreply.github.com>
2021-06-26 15:21:48 +1200

    Crude card identification

--- card-identifier.rb
+++ card-identifier.rb
@@ -2,6 +2,28 @@
 
 # CardIdentifier
 # Identifies flybuys cards from their numbers.
-class CardIndetifier
+class CardIdentifier
+  PREFIXES = {
+    'Black' => '60141',
+    'Red =>' => '6014352',
+    'Green_a' => '6014355526',
+    'Green_b' => '6014355529',
+    'Blue' => '6014'
+  }
 
+  def initialize(number)
+    @number = number
+  end
+
+  def identify
+    identified_card = ''
+    
+    PREFIXES.each do |key, value|
+      if @number.start_with?(value)
+        identified_card = "Fly Buys #{key}: #{@number}"
+      end
+    end
+
+    identified_card
+  end
 end
--- card-validator.rb
+++ card-validator.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require './card-identifier.rb'
+
 # CardValidator
 # Command line instructions: pass a file containing a list of card numbers to
 # validate, one set of numbers per line e.g.
@@ -8,12 +10,16 @@ class CardValidator
   def initialize
     file = File.open(ARGV.first)
     @card_numbers = file.read
-    puts "Card numbers"
-    puts @card_numbers
   end
 
   def main
+    identified_cards = @card_numbers.split(/\n/).map do |number|
+      CardIdentifier.new(number).identify
+    end
 
+    identified_cards.each do |card|
+      puts card
+    end
   end
 end
 
